73b07eb45afd69f259c2a28cae3db1d6888d25a6
Erick Penazzi <djerickdev@gmail.com>
2024-08-27 14:27:25 -0300

    Update 2

--- PrimeiroMVC/Controllers/AlunoController.cs
+++ PrimeiroMVC/Controllers/AlunoController.cs
@@ -8,10 +8,13 @@ namespace PrimeiroMVC.Controllers
         public IActionResult Index()
 
         {
-            Aluno a1 = new Aluno(1, "Jermilson", "Jermdocapa@gmail.com", 9078);
-            Aluno a2 = new Aluno(2, "Hegtar", "Hdogeloinvertido@gmail.com", 9077);
-            Aluno a3 = new Aluno(3, "Vausta", "VavaMainSage@gmail.com", 9076);
-            Aluno a4 = new Aluno(4, "Lucivalda", "Luluparedeinquebravel@gmail.com", 9075);
+            Aluno a1 = new Aluno(1, "Jermilson ", "Jermdocapa@gmail.com ", 9078);
+            Aluno a2 = new Aluno(2, "Hegtar ", "Hdogeloinvertido@gmail.com ", 9077);
+            Aluno a3 = new Aluno(3, "Vausta ", "VavaMainSage@gmail.com ", 9076);
+            Aluno a4 = new Aluno(4, "Lucivalda ", "Luluparedeinquebravel@gmail.com ", 9075);
+
+
+            //List para adicioanr os Aluno
 
             List<Aluno> ListarAluno = new List<Aluno>();
 
